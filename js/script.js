4243c71354e4035a1d36eab80b62e25d318576e9
vlad-a <58586793+vlad-a@users.noreply.github.com>
2024-09-07 00:56:12 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -265,4 +265,10 @@ $(document).ready(function () {
     };
     update();
   });
+  var $round = $(".round"),
+    roundRadius = $round.find("circle").attr("r"),
+    roundPercent = $round.data("percent"),
+    roundCircum = 2 * roundRadius * Math.PI,
+    roundDraw = (roundPercent * roundCircum) / 100;
+  $round.css("stroke-dasharray", roundDraw + " 999");
 });
